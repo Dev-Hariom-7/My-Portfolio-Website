de4ec794b2b0b1c7337be9a2c761a45cc952f53c
Hariom <hariomkumar7077@gmail.com>
2023-06-21 18:34:01 +0530

    changes in design

---
+++ js/app.js
@@ -0,0 +1,10 @@
+let typed = new Typed(".specs", {
+  strings: [
+    "JavaScript Developer.",
+    "ReactJS Developer.",
+    "Front-End Developer.",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
